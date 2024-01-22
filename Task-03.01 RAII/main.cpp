ec8c5d111248749a3b846335235bb6197d0afce4
Margaret Alade <alade.margaret@yandex.ru>
2024-01-22 06:44:59 +0300

    Initial Commit

---
+++ Task-03.01 RAII/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-03.01 RAII
+//
+//  Created by Маргарет  on 22.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
